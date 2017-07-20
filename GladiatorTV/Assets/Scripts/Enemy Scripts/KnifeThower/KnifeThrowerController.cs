4c7bc76cd5f3dcbfac1db40db1190716953f121c
Richard <ricardo.cantu03@utrgv.edu>
2017-07-19 22:14:40 -0500

    Teleporting knifeThrower
    Added the code for the KnifeThrower to teleport to a location every
    random x second between y - z
    Added the animations to this teleportation
    
    ******TELEPORT CURRENTLY ONLY TELEPORTS THE KNIFETHROWER TO THE RIGHT
    ONE UNIT UNTIL WE DECIDE WHAT POSITIONS AND HOW WE WILL DECIDE THIS
    POSITION

--- GladiatorTV/Assets/Scripts/Enemy Scripts/KnifeThower/KnifeThrowerController.cs
+++ GladiatorTV/Assets/Scripts/Enemy Scripts/KnifeThower/KnifeThrowerController.cs
@@ -10,6 +10,15 @@ public class KnifeThrowerController : BaseEnemy {
     private float cooldownCounter;
     private bool coolDown;
     private SpriteRenderer Sprite;
+
+    public float minTeleportTime;
+    public float maxTeleportTime;
+    public float currentTeleportTime;
+    public bool teleporting = false;
+    public float teleportCounter;
+
+    public Vector3 teleportLocation;
+    public bool routineCalled = false;
     // Use this for initialization
     void Start () {
         InvokeRepeating("Increment_Counters", 0f, .25f);
@@ -18,6 +27,7 @@ public class KnifeThrowerController : BaseEnemy {
         anim = this.gameObject.GetComponent<Animator>();
         anim.SetBool("Attacking", true);
         Sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        New_Teleport_Time();
     }
 
     // Update is called once per frame
@@ -28,20 +38,38 @@ public class KnifeThrowerController : BaseEnemy {
         Check_If_Dead();
 
 
-        if (Get_Attack_Counter() >= Get_Attack_Time())
+        if (teleportCounter >= currentTeleportTime && !Get_Attacking())
         {
-            SpawnProjectile(Knife);
-            Reset_Attack_Counter();
-            anim.SetBool("Attacking", false);
-            coolDown = true;
-            Set_Attacking(false);
+            if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            {
+                Reset_Attack_Counter();
+                cooldownCounter = 0f;
+                Set_Attacking(false);
+                if (!routineCalled)
+                {
+                    StartCoroutine(Start_Teleport());
+                    routineCalled = true;
+                }
+
+            }
         }
-        if(cooldownCounter == cooldownTime)
+        else if(!teleporting)
         {
-            cooldownCounter = 0f;
-            coolDown = false;
-            anim.SetBool("Attacking", true);
-            Set_Attacking(true);
+            if (Get_Attack_Counter() >= Get_Attack_Time())
+            {
+                SpawnProjectile(Knife);
+                Reset_Attack_Counter();
+                anim.SetBool("Attacking", false);
+                coolDown = true;
+                Set_Attacking(false);
+            }
+            if (cooldownCounter == cooldownTime)
+            {
+                cooldownCounter = 0f;
+                coolDown = false;
+                anim.SetBool("Attacking", true);
+                Set_Attacking(true);
+            }
         }
     }
 
@@ -73,7 +101,7 @@ public class KnifeThrowerController : BaseEnemy {
 
     private void Increment_Counters()
     {
-        if (Get_Attacking())
+        if (Get_Attacking() && !teleporting)
         {
             Increase_Attack_Counter(.25f);
         }
@@ -81,9 +109,13 @@ public class KnifeThrowerController : BaseEnemy {
         {
             Increase_Death_Counter(.25f);
         }
-        if (coolDown)
+        if (coolDown && !teleporting)
         {
             cooldownCounter += .25f;
+        }
+        if(!teleporting)
+        {
+            teleportCounter += .25f;
         }
     }
 
@@ -128,5 +160,30 @@ public class KnifeThrowerController : BaseEnemy {
                 
             }
         }
+    }
+
+    private void New_Teleport_Time()
+    {
+        currentTeleportTime = (int)Random.Range(minTeleportTime, maxTeleportTime);
+    }
+
+    private IEnumerator Start_Teleport()
+    {
+        Set_Attacking(false);
+        teleporting = true;
+        anim.SetBool("Teleporting", true);
+        Reset_Attack_Counter();
+        teleportCounter = 0f;
+        yield return new WaitForSeconds(.75f);
+        Teleport_To_Location(this.gameObject.transform.position);
+        anim.SetBool("Teleporting", false);
+        yield return new WaitForSeconds(1f);
+        teleporting = false;
+        routineCalled = false;
+    }
+
+    private void Teleport_To_Location(Vector3 point)
+    {
+        this.gameObject.transform.position = point + new Vector3(1,0,0);
     }
 }
