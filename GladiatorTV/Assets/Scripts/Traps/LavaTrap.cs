7ffddff6065f9bd2b9fd46f41d11ad6be4536972
Richard <ricardocantuii03@gmail.com>
2017-07-21 14:22:37 -0500

    spike,lava,shock trap all added
    Currently the spike lava and shock trap are working, but they only respond to the player. need to add functionality to with the enemy.
    
    also need to work on bear trap

---
+++ GladiatorTV/Assets/Scripts/Traps/LavaTrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTrap : MonoBehaviour {
+    public int trapDamage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player_con = collision.GetComponent<PlayerController>();
+            player_con.Deal_Damage(trapDamage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player_con = collision.GetComponent<PlayerController>();
+            player_con.Deal_Damage(trapDamage);
+        }
+    }
+}
---
+++ GladiatorTV/Assets/Scripts/Traps/ShockTrap.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockTrap : MonoBehaviour {
+    public int trapDamage;
+
+    public bool active;
+
+    public float minTime;
+    public float maxTime;
+    public float currentTimer;
+
+    public float counter;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Increment_Counters", 0f, .25f);
+        Get_New_Time();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(counter >= currentTimer)
+        {
+            Change_State();
+            counter = 0;
+            Get_New_Time();
+        }
+
+	}
+
+    private void Increment_Counters()
+    {
+        counter += .25f;
+    }
+
+    private void Change_State()
+    {
+        if(active)
+        {
+            
+        }
+        else
+        {
+
+        }
+        active = !active;
+    }
+
+    private void Get_New_Time()
+    {
+        currentTimer = (int)Random.Range(minTime, maxTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && active)
+        {
+            PlayerController player_con = collision.GetComponent<PlayerController>();
+            player_con.Deal_Damage(trapDamage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && active)
+        {
+            PlayerController player_con = collision.GetComponent<PlayerController>();
+            player_con.Deal_Damage(trapDamage);
+        }
+    }
+}
