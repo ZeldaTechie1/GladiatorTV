99e71f1b03d2ab11fbb71503548774e49c3e5a9d
Richard <ricardocantuii03@gmail.com>
2017-07-24 22:33:52 -0500

    spike floor now added

---
+++ GladiatorTV/Assets/Scripts/BossRoom/Spikes/SpikeFloor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeFloor : MonoBehaviour {
+
+    public int trapDamage;
+
+    public float delayTime;
+    public float activeTime;
+
+    public bool triggered;
+    public bool activated;
+
+    public float counter;
+    public int row = 0;
+
+    private void Awake()
+    {
+        InvokeRepeating("incrementCounter", 0f, .25f);
+        SetValues();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(triggered)
+        {
+            CheckRow();
+            activaterows();
+        }
+	}
+
+
+    public void SetValues()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<SpikeRow>().activeTime = activeTime;
+            child.GetComponent<SpikeRow>().delayTime = delayTime;
+            child.GetComponent<SpikeRow>().trapDamage = trapDamage;
+        }
+    }
+
+    private void activaterows()
+    {
+        int current_row = 0;
+        foreach (Transform child in transform)
+        {
+            Debug.Log(row);
+            Debug.Log(current_row);
+            if(current_row == row)
+            {
+                child.GetComponent<SpikeRow>().triggered = true;
+                if(current_row == 8)
+                {
+                    triggered = false;
+                    row = 0;
+                }
+            }
+            current_row++;
+
+        }
+    }
+
+    private void CheckRow()
+    {
+        if(counter >= delayTime)
+        {
+            row++;
+            counter = 0;
+        }
+    }
+
+    private void incrementCounter()
+    {
+        if(triggered)
+        {
+            counter += .25f;
+        }
+    }
+}
---
+++ GladiatorTV/Assets/Scripts/BossRoom/Spikes/SpikeRow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeRow : MonoBehaviour {
+
+    public int trapDamage;
+
+    public float delayTime;
+    public float activeTime;
+
+    public bool triggered;
+    public bool activated;
+
+    public float counter;
+
+    // Use this for initialization
+    void Start () {
+        SetValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggered)
+        {
+            ActivateSpikes();
+            triggered = false;
+        }
+	}
+
+    public void ActivateSpikes()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<BossSpike>().triggered == false && child.GetComponent<BossSpike>().activated == false)
+                child.GetComponent<BossSpike>().triggered = true;
+        }
+    }
+
+    public void SetValues()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<BossSpike>().activeTime = activeTime;
+            child.GetComponent<BossSpike>().delayTime = delayTime;
+            child.GetComponent<BossSpike>().trapDamage = trapDamage;
+        }
+    }
+}
