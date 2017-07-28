cdb172c9f2b4a4bac72a13c59e195bc0287229ef
Zeldatechie <jesrey4@outlook.com>
2017-07-28 00:17:09 -0500

    Intro Scene completo

--- GladiatorTV/Assets/Scripts/General Scripts/Intro.cs
+++ GladiatorTV/Assets/Scripts/General Scripts/Intro.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 
 public class Intro : MonoBehaviour {
 
-    
+
+    public AudioSource backgroundMusic;
     public AudioSource announcerIntro;
     public AudioSource oldPeopleIntro;
 
+    public GameObject muteButton;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        muteButton.SetActive(false);
+        StartCoroutine(StartIntro());
+
 	}
 	
-	
+	IEnumerator StartIntro()
+    {
+        announcerIntro.Play();
+        yield return new WaitForSeconds(49f);
+        backgroundMusic.Stop();
+        muteButton.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        oldPeopleIntro.Play();
+        yield return new WaitForSeconds(16f);
+        sceneDone();
+
+    }
 
     void sceneDone()
     {
