299b5a18467dae6d43056fafd234aa6f9222ed69
Zeldatechie <jesrey4@outlook.com>
2017-07-27 20:28:04 -0500

    Flavor text for objective drops added

---
+++ GladiatorTV/Assets/Scripts/General Scripts/ObjectivePickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePickup : MonoBehaviour {
+
+    [SerializeField]
+    string flavorText;
+
+    public string getFlavorText()
+    {
+        return flavorText;
+    }
+}
